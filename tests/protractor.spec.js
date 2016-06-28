d599b0519fc72bc181d2db05b3505fb76d9dafc1
Will Klein <will.klein@gmail.com>
2016-06-28 17:40:46 +0100

    Add comments for first test to write

--- tests/protractor.spec.js
+++ tests/protractor.spec.js
@@ -4,4 +4,16 @@ describe('TodoMVC app', function() {
     var title = browser.getTitle();
     expect(title).toBe('Angular2 • TodoMVC');
   });
+
+  it('should add a todo', function() {
+    browser.get('http://localhost:8080/');
+
+    // get the input box
+    // enter todo description
+    // enter RETURN
+
+    // validate the todo item is in the list
+    // look up the todo item in the list
+    // expect the text of the list item to be the todo description entered
+  });
 });
