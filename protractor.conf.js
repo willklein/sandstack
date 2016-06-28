4b476c3a17e2ac225c31e32a22cea1195b4d9924
Will Klein <will.klein@gmail.com>
2016-06-28 17:12:54 +0100

    Rename todomvc.spec.js to protractor.spec.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,5 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['tests/todomvc.spec.js'],
+  specs: ['tests/protractor.spec.js'],
   useAllAngular2AppRoots: true
 };
