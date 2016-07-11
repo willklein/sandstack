a06db6a83dadf1decced1c5292f26e1a5b715d4a
wrstout <wrstout55@gmail.com>
2016-07-11 17:01:39 -0600

    first commit to billy's branch
    - updated user settings for saving and formatting
    - implemented page Object model for the tests
    - created pageObject.js to facilitate the implementation of Page Object

--- tests/protractor.spec.js
+++ tests/protractor.spec.js
@@ -1,31 +1,64 @@
-describe('TodoMVC app', function() {
-  var newTodo;
+var TodoHomepage = require('./pageObject.js');
+describe('TodoMVC app', function () {
+  var todoHomepage;
 
-  beforeEach(() => {
-    browser.get('http://localhost:8080/');
-
-    newTodo = element(by.css('.new-todo'));
+  beforeEach(function () {
+    todoHomepage = new TodoHomepage();
   });
 
-  it('should have the right title', () => {
-    var title = browser.getTitle();
-    expect(title).toBe('Typescript & AngularJS • TodoMVC');
+  it('should have the right title', function () {
+    expect(todoHomepage.title).toBe('Typescript & AngularJS • TodoMVC');
   });
 
-  it('should have placeholder text in the new todo input', () => {
-    var placeholderText = newTodo.getAttribute('placeholder');
-    expect(placeholderText).toEqual('What needs to be done?');
+  it('should have placeholder text in the new todo input', function () {
+    expect(todoHomepage.placeholderText).toEqual('What needs to be done?');
   });
 
-  it('should add a new todo', () => {
-    // enter todo description
-    // enter RETURN
-
-    // validate the todo item is in the list
-    // look up the entered todo item in the list
-
+  it('should add a new todo', function () {
+    todoHomepage.addTodo('todoOne');
+    expect(todoHomepage.todoList.count()).toEqual(1);
     // expect the text of the list item to be the todo description entered
+    expect(todoHomepage.description).toEqual('todoOne');
   });
 
-  // what other tests do you want to write? i.e. it('should ...', () => {});
+  //there is one TODO existing in the list currently
+  it('should remove deleted todos from list', function () {
+    // Hover the only existing todo // click the delete button
+    todoHomepage.deleteTodo('todoOne');
+    // Verify no items are in list
+    expect(todoHomepage.todoList.count()).toEqual(0);
+  });
+
+  // the TODO list is now empty
+  it('should only show Active todos in Active list', function () {
+    todoHomepage.addTodo('Test Description');
+    // click Active href
+    todoHomepage.activeButton.click();
+    // Verify that there is one in the active list
+    expect(todoHomepage.activeItems.count()).toEqual(1);
+  });
+
+  // there is now one item in the ALL and ACTIVE list, that will be 'checked' then verified as completed
+  it('should only show Completed todos in Completed list', function () {
+    // click completed checkbox
+    todoHomepage.checkbox.click();
+    // click Completed Link
+    todoHomepage.completedButton.click();
+    // verify there is one item in the completed list
+    expect(todoHomepage.completedItems.count()).toEqual(1);
+  }, 10000); //this is how to set a timeout, there may be a 5 second timeout on tests.
+
+
+  it("should remove all list items when all are selected and cleared", function () {
+    // enter todo description
+    todoHomepage.addTodo('001');
+    // enter todo description
+    todoHomepage.addTodo('002');
+    //get and click the select all todos button
+    todoHomepage.checkAllButton.click();
+    //click clear completed
+    todoHomepage.clearCompletedButton.click();
+    //verify that 'Clear Completed' button no longer exists
+    expect(todoHomepage.clearCompletedButton.toBe == null);
+  });
 });
